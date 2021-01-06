374186be913b2cb8a9a9c1341689ef6bf4d6365e
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2021-01-06 13:01:44 -0800

    Create Form.js

---
+++ src/components/Form.js
@@ -0,0 +1,31 @@
+import React from 'react'
+class ChangeNameForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+          value: ''
+        };
+    }
+    handleChange = (event) => {
+        console.log(this.state);
+        this.setState({
+            value: event.target.value
+        });
+    };
+    handleSubmit = (event) => {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+           <input type="text" value={this.state.value} onChange={this.handleChange} />
+         </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+}
+export default ChangeNameForm
